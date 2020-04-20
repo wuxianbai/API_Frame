6665e46bd2a50d6dbf3d585617c3681a9e55860f
wuxian <454762930@qq.com>
2020-04-21 00:27:06 +0800

    练习下修改后提交

--- util/ToJson.py
+++ util/ToJson.py
@@ -3,7 +3,7 @@ import re
 
 def qrCodeToJson(strs):
     '''
-    为了针对不同的返回内容，做了不同的处理,该方法是因为银联接口返回的字段中包括signature字段也包含=，会导致使用=分割错误
+    为了针对不同的返回内容，做了不同的处理,该方法是因为银联接口返回的字段中包括signature字段也包含=，会导致使用=分割错误.
     :param strs:
     :return:
     '''
