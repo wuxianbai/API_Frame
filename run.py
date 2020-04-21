6bfa59d4eaf2aa8230e5b1a4a2fc71362b9df9c1
wuxian <454762930@qq.com>
2020-04-21 12:49:11 +0800

    完善自动化用例执行完成后自动发送邮件

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
-import unittest
+import unittest, os
 from BeautifulReport import BeautifulReport
 import common.GlobalVars as gv
+import util.SendEmail as se
 
 if __name__ == '__main__':
     gv._init()
     suite = unittest.defaultTestLoader.discover('./testcase/', 'qrCode.py')
     report = BeautifulReport(suite)
     report.report(description='接口自动化', report_dir='./report/', filename='report.html')
+
+    sender = '454762930@qq.com'
+    pwd = 'iyayyxtpocqjcacb'
+    receiver = '454762930@qq.com'
+    smtpserver = 'smtp.qq.com'
+    port = 465
+    report = os.path.join(os.path.dirname(os.path.realpath(__name__)), 'report', 'report.html')
+    se.send_email(sender, pwd, receiver, smtpserver, port, report)
